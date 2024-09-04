c25092a3eb99c5c5c01f72aca1ac33c33d98677f
Alan-Nixon <alannixon2520@gmail.com>
2024-09-04 18:36:48 +0530

    added validations

---
+++ src/Default.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Alert from '@mui/material/Alert'
+
+export default function DefaultExport() { 
+  return <Alert severity="info">Please destructure the methods and use we dont have default export  😝 😝 😝</Alert>
+}
+
+
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
-import MyComponent from './Validations';
+import MyComponent from './Default';
+import * as validations from './validations';
+
+export const { validateName, validateFullName, validateEmail, validatePassword, validatePhone } = validations;
+export const { validateURL, validateYouTubeURL, validateMongoObjectId, validateCreditCard, validateMACAddress } = validations
+export const { validateIPV4, validateIPv6, validateDate, validateHexColor, validateUUID } = validations
+export const { validateCreditCardCVV, validateHTMLTag, validateCountryCode, validatePassportNumber, validateBinary } = validations
+export const { validateLatitude, validateLongitude, } = validations
 
 export default MyComponent;
