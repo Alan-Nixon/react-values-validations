64a147dc7c9e50c0018c8b38d3b6beee820df7e5
Alan-Nixon <alannixon2520@gmail.com>
2024-09-04 18:05:07 +0530

    emoji added

--- src/Validations.tsx
+++ src/Validations.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Alert from '@mui/material/Alert'
 
 function Validations() {
   return (
     <div>
-      hello world i am Validation of alan nixon paliakkara
+      <Alert severity="info">Please destructure the methods and use we dont have default export  😝 😝 😝</Alert>
+      
     </div>
   )
 }
